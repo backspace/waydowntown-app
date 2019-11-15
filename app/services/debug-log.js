dee543b6e8de56835e48996b93ceb1ecc1c5f2ac
Buck Doyle <b@chromatin.ca>
2019-11-15 16:36:02 -0600

    Change log to be reverse-sorted

--- app/services/debug-log.js
+++ app/services/debug-log.js
@@ -5,6 +5,6 @@ export default class DebugLogService extends Service {
   @tracked entries = [];
 
   log(entry) {
-    this.entries = [...this.entries, entry];
+    this.entries = [entry, ...this.entries];
   }
 }
