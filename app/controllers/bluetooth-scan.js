5c764ba620f5089b694936e8931f57794a8ddd27
Buck Doyle <b@chromatin.ca>
2019-11-14 21:11:53 -0600

    Add prototype Bluetooth Scan route

---
+++ app/controllers/bluetooth-scan.js
@@ -0,0 +1,41 @@
+import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { later } from '@ember/runloop';
+
+export default class ApplicationController extends Controller {
+  @tracked devices = [];
+  @tracked status = 'unknown';
+
+  deviceSet = new Set();
+
+  @tracked error;
+
+  @action
+  search() {
+    window.bluetoothle.initialize(result => {
+      this.status = result.status;
+
+      if (result.status === 'enabled') {
+        window.bluetoothle.isScanning(({isScanning}) => {
+          if (isScanning) {
+            this.error = 'Already scanning';
+          } else {
+            later(this, () => {
+              window.bluetoothle.stopScan(() => {}, () => {});
+            }, 5000);
+
+            window.bluetoothle.startScan(result => {
+              if (result.status === 'scanResult') {
+                this.deviceSet.add(result.name);
+                this.devices = Array.from(this.deviceSet);
+              }
+            }, error => {
+              this.error = JSON.stringify(error);
+            }, { services: [] });
+          }
+        })
+      }
+    });
+  }
+}
