540bd7b661b6bc482b17f52b4ba228b02b9804ad
Buck Doyle <b@chromatin.ca>
2019-11-29 08:53:19 -0600

    Update adapter to use correctly-scoped token

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,12 +1,15 @@
 import DS from 'ember-data';
 import config from 'waydowntown/config/environment';
+import { storageFor } from 'ember-local-storage';
 
 export default class ApplicationAdapter extends DS.JSONAPIAdapter {
   host = config.APP.server;
 
+  @storageFor('token') tokenStorage;
+
   get headers() {
     return {
-      Authorization: `Bearer ${localStorage.getItem('token')}`,
+      Authorization: `Bearer ${this.get('tokenStorage.token')}`,
     };
   }
 }
