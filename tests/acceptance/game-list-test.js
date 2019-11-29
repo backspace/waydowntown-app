398b709cfb5c3d7eab70ecc36d7d4efaa297ae17
Buck Doyle <b@chromatin.ca>
2019-11-28 22:16:42 -0600

    Combine and rename games list tests

--- tests/acceptance/game-list-test.js
+++ tests/acceptance/game-list-test.js
@@ -1,15 +1,36 @@
 import { module, test } from 'qunit';
-import { settled, visit } from '@ember/test-helpers';
+import { click, settled, visit } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 import setToken from '../set-token';
 import mockCable from '../mock-cable';
 
-module('Acceptance | game invitation receipt', function(hooks) {
+module('Acceptance | game list', function(hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
   setToken(hooks);
   mockCable(hooks);
+
+  test('a requested game is displayed as an invitation', async function(assert) {
+    const concept = this.server.create('concept', {
+      name: 'a requested concept',
+    });
+    const incarnation = concept.createIncarnation();
+    const game = incarnation.createGame();
+    game.createParticipation({
+      team: this.team,
+    });
+
+    await visit('/');
+    await click('[data-test-request]');
+    await settled();
+
+    assert
+      .dom(
+        `[data-test-invitations] [data-test-game-id='${game.id}'] [data-test-concept-name]`,
+      )
+      .hasText('a requested concept');
+  });
 
   test('a received invitation is displayed', async function(assert) {
     const concept = this.server.create('concept', {
@@ -43,13 +64,6 @@ module('Acceptance | game invitation receipt', function(hooks) {
       )
       .hasText('an invited concept');
   });
-});
-
-module('Acceptance | game invitation and acceptance listing', function(hooks) {
-  setupApplicationTest(hooks);
-  setupMirage(hooks);
-  setToken(hooks);
-  mockCable(hooks);
 
   test('existing invitations and acceptances are listed', async function(assert) {
     const otherTeam = this.server.create('team', { name: 'other team' });
