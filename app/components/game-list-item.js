503414eec4b727a6030d7fac6e86f1d1ac14a1bd
Buck Doyle <b@chromatin.ca>
2019-11-28 22:05:37 -0600

    Add other team names to game list items

--- app/components/game-list-item.js
+++ app/components/game-list-item.js
@@ -1,5 +1,9 @@
 import Component from '@glimmer/component';
 
 export default class GameListItem extends Component {
-
+  get otherTeams() {
+    return this.args.game.participations
+      .mapBy('team')
+      .rejectBy('id', this.args.team.id);
+  }
 }
--- tests/acceptance/invitations-test.js
+++ tests/acceptance/invitations-test.js
@@ -52,13 +52,19 @@ module('Acceptance | game invitation and acceptance listing', function(hooks) {
   mockCable(hooks);
 
   test('existing invitations and acceptances are listed', async function(assert) {
+    const otherTeam = this.server.create('team', { name: 'other team' });
+    const thirdTeam = this.server.create('team', { name: 'a third team' });
+
     const concept = this.server.create('concept', {
       name: 'an invited concept',
     });
     const incarnation = concept.createIncarnation();
     const game = incarnation.createGame();
     game.createParticipation({
-      team: this.server.create('team', { name: 'other team' }),
+      team: otherTeam,
+    });
+    game.createParticipation({
+      team: thirdTeam,
     });
     game.createParticipation({ team: this.team });
 
@@ -79,6 +85,19 @@ module('Acceptance | game invitation and acceptance listing', function(hooks) {
         `[data-test-invitations] [data-test-game-id='${game.id}'] [data-test-concept-name]`,
       )
       .hasText('an invited concept');
+    assert
+      .dom(
+        `[data-test-invitations] [data-test-team-id='${otherTeam.id}'] [data-test-team-name]`,
+      )
+      .hasText('other team');
+    assert
+      .dom(
+        `[data-test-invitations] [data-test-team-id='${thirdTeam.id}'] [data-test-team-name]`,
+      )
+      .hasText('a third team');
+    assert
+      .dom(`[data-test-invitations] [data-test-team-id='${this.team.id}']`)
+      .doesNotExist();
 
     assert
       .dom(`[data-test-invitations] [data-test-game-id='${acceptedGame.id}']`)
