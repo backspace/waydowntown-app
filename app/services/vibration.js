5d33a78e729325597befcf7985e683108c0b86e1
Buck Doyle <b@chromatin.ca>
2019-12-03 16:26:34 -0600

    Add guard against missing vibration

--- app/services/vibration.js
+++ app/services/vibration.js
@@ -4,6 +4,8 @@ import { action } from '@ember/object';
 export default class VibrationService extends Service {
   @action
   vibrate() {
-    navigator.vibrate(100);
+    if (navigator.vibrate) {
+      navigator.vibrate(100);
+    }
   }
 }
