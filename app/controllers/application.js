bc771f2e3b973b1f014fa90039b6a31392298bda
Buck Doyle <b@chromatin.ca>
2019-12-07 12:00:55 -0600

    Move URL to diagnostics and add API

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,14 +1,23 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
+import config from 'waydowntown/config/environment';
+import { tracked } from '@glimmer/tracking';
 import { storageFor } from 'ember-local-storage';
 
 export default class ApplicationController extends Controller {
   @service debugLog;
   @storageFor('token') tokenStorage;
 
+  @tracked showDiagnostics = false;
+  @tracked api;
+  @tracked url;
+
   @action
-  url() {
-    this.debugLog.log(window.location);
+  toggleDiagnostics() {
+    this.url = window.location;
+    this.api = config.APP.server;
+
+    this.showDiagnostics = !this.showDiagnostics;
   }
 }
