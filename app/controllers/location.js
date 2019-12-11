beae22fd753e93b2f9e1702c087ab88761e88fd0
Buck Doyle <b@chromatin.ca>
2019-12-10 20:50:12 -0600

    Add preliminary member location-saving

--- app/controllers/location.js
+++ app/controllers/location.js
@@ -1,8 +1,10 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
+import { inject as controller } from '@ember/controller';
 import { action } from '@ember/object';
 
 export default class LocationController extends Controller {
+  @controller('index') indexController;
   @tracked event = [];
 
   @action
@@ -10,6 +12,15 @@ export default class LocationController extends Controller {
     this.watchId = navigator.geolocation.watchPosition(position => {
       this.event = position.coords;
       this.event.date = new Date(position.timestamp);
+
+      if (this.indexController.member) {
+        const member = this.indexController.member;
+        member.setProperties({
+          lat: position.coords.latitude,
+          lon: position.coords.longitude,
+        });
+        member.save();
+      }
     });
   }
 
