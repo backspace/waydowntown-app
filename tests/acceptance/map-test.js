12e5408f9a5a09814600fccbf2fd1d87ba6c8144
Buck Doyle <b@chromatin.ca>
2019-12-26 22:30:30 -0600

    Remove unused import

--- tests/acceptance/map-test.js
+++ tests/acceptance/map-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { click, fillIn, settled, visit } from '@ember/test-helpers';
+import { click, settled, visit } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 import setToken from 'waydowntown/tests/helpers/set-token';
