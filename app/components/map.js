da84bfc8d2d0d353de74812b7314dc875e7f6050
Buck Doyle <b@chromatin.ca>
2019-12-29 22:28:53 -0600

    Add configurable map tile layer

--- app/components/map.js
+++ app/components/map.js
@@ -1,7 +1,16 @@
 import Component from '@glimmer/component';
+import L from 'leaflet';
+import ENV from 'waydowntown/config/environment';
 
 export default class Map extends Component {
   lat = 49.8913444;
   lon = -97.1429387;
   zoom = 15;
+
+  bounds = L.latLngBounds(
+    L.latLng(49.885707, -97.152909),
+    L.latLng(49.897725, -97.134089),
+  );
+
+  mapTileUrl = ENV.MAP_TILE_URL;
 }
